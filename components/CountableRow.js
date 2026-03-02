1d3dab29b126998b0a81e6cd4ab8d1569f7bc26a
Joakim Isaksson <joakim.isaksson@ha.ax>
2026-03-02 10:54:10 +0200

    Initial commit

---
+++ components/CountableRow.js
@@ -0,0 +1,36 @@
+import { Text, View, StyleSheet } from "react-native";
+import { CountButton } from "./CountButton";
+import { CommonStyles } from "../styles/CommonStyles";
+
+export const CountableRow = ({ countable, changeCount, index }) => (
+  <View style={CommonStyles.row}>
+    <View style={styles.nameColumn}>
+      <Text style={CommonStyles.textItem}>{countable.name}</Text>
+      <Text style={CommonStyles.textItem}>{countable.count}</Text>
+    </View>
+    <View style={styles.buttonColumn}>
+      <CountButton
+        text={"+"}
+        submit={() => {
+          changeCount(1, index);
+        }}
+      />
+      <CountButton
+        text={"-"}
+        submit={() => {
+          changeCount(-1, index);
+        }}
+      />
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  nameColumn: {
+    flex: 0.8,
+    alignItems: "center",
+  },
+  buttonColumn: {
+    flex: 0.2,
+  },
+});
